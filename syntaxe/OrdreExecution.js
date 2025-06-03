c1b378c687fd1b844faf2a797e8a6bac8fd3f71f
PeyreGuillaume34 <peyreguillaume34@gmail.com>
2025-06-03 21:02:26 +0200

    ordre execution

---
+++ syntaxe/OrdreExecution.js
@@ -0,0 +1,20 @@
+
+// Que vaudront chacune des variables 
+// firstLanguage, secondLanguage  et thirdLanguage  à la fin du code suivant :
+let firstLanguage = "HTML";
+let secondLanguage = "PHP";
+let thirdLanguage = firstLanguage ;
+firstLanguage  = "Javascript";
+secondLanguage = firstLanguage ;
+
+firstLanguage (Javascript), secondLanguage(Javascript)  et thirdLanguage (HTML)
+
+
+// Et dans ce cas-ci :
+let firstLanguage  = "HTML";
+let secondLanguage  = "PHP";
+firstLanguage  = "Javascript";
+let thirdLanguage  = firstLanguage ;
+secondLanguage  = firstLanguage ;
+
+firstLanguage (Javascript), secondLanguage(Javascript) et thirdLanguage (Javascript)
