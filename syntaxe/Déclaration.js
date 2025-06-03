1b1e95c1ca2a9ecd34e8ac9321b32f3c9afade88
PeyreGuillaume34 <peyreguillaume34@gmail.com>
2025-06-03 15:01:38 +0200

    Déclaration

---
+++ syntaxe/Déclaration.js
@@ -0,0 +1,26 @@
+
+//1) En javascript, déclare 2 variables dans lesquelles
+// tu affecteras respectivement ton nom et prénom (choisis un nom pertinent pour chaque variable !).
+// Avec l’instruction alert(……), tu afficheras "Bonjour, nom prénom".
+
+const chiffre = 64
+let age = 42
+let name = "Peyre"
+let prenom = "Guillaume"
+
+
+alert("Bonjour"+ " " + name + " " + prenom)
+alert("Il te reste" + " " +(chiffre-age) + " " 
++ "Années à travailler avant la retraite ! Courage ! En développant ce sera plus fun ! ")
+
+
+
+/*
+let student = {
+		name : "Ada",
+		age : "999",
+		learning : "coding"
+	}
+    console.log(student)
+
+*/
