a04101c0c96ed4fdc94e1fc376621f8b448011aa
PeyreGuillaume34 <peyreguillaume34@gmail.com>
2025-06-09 16:22:08 +0200

    exercices

---
+++ function/exercice1.js
@@ -0,0 +1,33 @@
+
+// function addition3Numbers(number1,number2,number3){
+//     return `Le résultat de la somme est ${number1+number2+number3}`
+// }
+
+// console.log(addition3Numbers(1,2,3))
+
+
+// let A = 1
+// let B = 4
+// let C = 3
+
+// function addition3NumbersWithArg(number1,number2,number3){
+//     return  `Le résultat de la somme est ${number1+number2+number3}`
+// }
+// console.log(addition3NumbersWithArg(A,B,C))
+
+
+let A = 1
+let B = 4
+let C = 3
+
+function addition3NumbersWithArg(number1,number2,number3){
+    return  `Le résultat de la somme est ${number1+number2+number3}`
+}
+console.log(addition3NumbersWithArg(A,B,C))
+A = 3 ; B = 34 ; C=12
+
+console.log(addition3NumbersWithArg(A,B,C))
+A=6 ; B=5 ; C=9
+
+console.log(addition3NumbersWithArg(A,B,C))
+A=9 ; B=7 ; C=9
