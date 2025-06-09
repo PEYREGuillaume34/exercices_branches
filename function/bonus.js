eabef0dc6e773a3e7ba7f40693912b6f101b0b96
PeyreGuillaume34 <peyreguillaume34@gmail.com>
2025-06-09 17:46:59 +0200

    Bonus

---
+++ function/bonus.js
@@ -0,0 +1,17 @@
+let farine = 250
+let lait = 500
+let oeuf = 3
+let sucre = 50
+
+function preparerPate(farine,lait,oeuf, sucre){
+return `Pâte prête avec ${farine}g de farine, ${lait}ml de lait, ${oeuf} oeufs et ${sucre}g de sucre.`
+
+}
+console.log(preparerPate(farine,lait,oeuf, sucre))
+
+
+function cuirecrepe(farine,lait,oeuf, sucre){
+return `Crêpe cuite avec ${farine}g de farine, ${lait}ml de lait, ${oeuf} oeufs et ${sucre}g de sucre.`
+
+}
+console.log(cuirecrepe(farine,lait,oeuf, sucre))
